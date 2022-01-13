f12caba484fd0f6704e09a68108319687cfc63de
Axorax <78349410+Axorax@users.noreply.github.com>
2022-01-14 00:13:37 +0600

    Update script.js

--- wp-content/script.js
+++ wp-content/script.js
@@ -1,9 +1,1 @@
-// © Pedint
-// Cool CSS Gradients!!!
-// to level up your website :)
-// Made with love by Axorax
-
-
-
-
 const load = document.getElementById('loading');function loadfun(){load.style.display = 'none';}function backToTop() {document.body.scrollTop = 0;document.documentElement.scrollTop = 0;}

5551ba2a2e1cbd0ce4cb4ee6eacf0fdadaf7c791
Axorax <78349410+Axorax@users.noreply.github.com>
2022-01-13 21:05:12 +0600

    Add files via upload

---
+++ wp-content/script.js
@@ -0,0 +1,11 @@
+// Pre-Loader
+const load = document.getElementById('loading');
+function loadfun(){
+    load.style.display = 'none';
+}
+
+// Back to top
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
